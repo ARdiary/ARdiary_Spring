a86e801543717f221ac2f4952e659461ce6ad70d
dldmldlsy <dldmldlsy@naver.com>
2022-06-24 20:53:19 +0900

    스프링 프로젝트 생성

---
+++ ardiary/src/test/java/com/army/ardiary/ArdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.army.ardiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
