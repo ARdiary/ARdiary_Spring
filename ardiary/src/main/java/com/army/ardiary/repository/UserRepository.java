3fb2e2732980dad9754d30530ccae0aff4478df4
dldmldlsy <dldmldlsy@naver.com>
2022-07-22 20:40:37 +0900

    user table CRUD 기능 구현

--- ardiary/src/main/java/com/army/ardiary/repository/UserRepository.java
+++ ardiary/src/main/java/com/army/ardiary/repository/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository {
     int insert(UserEntity userEntity);
-
+    UserEntity selectById(int userId);
+    int update(UserEntity userEntity);
+    int delete(int userId);
     UserEntity selectByEmail(String email);
 }
