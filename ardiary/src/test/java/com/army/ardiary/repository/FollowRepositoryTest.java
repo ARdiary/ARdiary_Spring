ddc0594c13d94d859adebd73a09ca625f9be4d74
prkim99@gmail.com <prkim99@gmail.com>
2022-08-03 00:21:20 +0900

    DB연결, swagger 추가 오류 개선(?)

--- ardiary/src/test/java/com/army/ardiary/repository/FollowRepositoryTest.java
+++ ardiary/src/test/java/com/army/ardiary/repository/FollowRepositoryTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
+@MapperScan(basePackages = "com.army.ardiary")
 class FollowRepositoryTest {
 
     @Autowired
