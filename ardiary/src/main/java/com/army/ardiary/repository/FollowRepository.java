c9eeb735fbc6f0cb5677f591b694f8cc33634012
dldmldlsy <dldmldlsy@naver.com>
2022-08-05 21:26:30 +0900

    FollowRepository, FollowMapper 줄간격변경(가독성)

--- ardiary/src/main/java/com/army/ardiary/repository/FollowRepository.java
+++ ardiary/src/main/java/com/army/ardiary/repository/FollowRepository.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface FollowRepository {
+
     int insert(FollowEntity followEntity);
+
     FollowEntity selectById(int followId);
+
     int update(FollowEntity followEntity);
+
     int delete(int followId);
-    List<FollowEntity> selectByFollower(int follower);
-    List<FollowEntity> selectByFollowee(int follower);
 }
