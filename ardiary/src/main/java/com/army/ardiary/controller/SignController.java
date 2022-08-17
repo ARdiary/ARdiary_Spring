69e2d1f5d564872f94bff4c1eeb05b9d8a7becc5
dldmldlsy <dldmldlsy@naver.com>
2022-08-17 21:00:51 +0900

    swagger 관련 설정: 의존성추가, SwaggerConfig 생성, controller에 어노테이션 추가, yml에 springfox가능하도록 설정 추가

--- ardiary/src/main/java/com/army/ardiary/controller/SignController.java
+++ ardiary/src/main/java/com/army/ardiary/controller/SignController.java
@@ -4,6 +4,7 @@ import com.army.ardiary.dto.EmailRequestDto;
 import com.army.ardiary.dto.ErrorResponse;
 import com.army.ardiary.dto.LoginResponseDto;
 import com.army.ardiary.service.SignService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "signUp controller", description = "bear controller desc") //swagger annotation
 public class SignController {
     // SignUp(회원가입) SignIn(로그인)API를 다루는 컨트롤러
 
