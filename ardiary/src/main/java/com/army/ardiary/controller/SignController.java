ce7450db51d457700ec7ebd4cfb3165d9fdf0097
dldmldlsy <dldmldlsy@naver.com>
2022-08-17 20:37:27 +0900

    swagger 설정 삭제

--- ardiary/src/main/java/com/army/ardiary/controller/SignController.java
+++ ardiary/src/main/java/com/army/ardiary/controller/SignController.java
@@ -4,7 +4,6 @@ import com.army.ardiary.dto.EmailRequestDto;
 import com.army.ardiary.dto.ErrorResponse;
 import com.army.ardiary.dto.LoginResponseDto;
 import com.army.ardiary.service.SignService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +15,6 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "signUp controller", description = "bear controller desc") //swagger annotation
 public class SignController {
     // SignUp(회원가입) SignIn(로그인)API를 다루는 컨트롤러
 
