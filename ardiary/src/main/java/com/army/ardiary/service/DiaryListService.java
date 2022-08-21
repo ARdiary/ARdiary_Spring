e2ef5fabfa11f57f0f1fc3711b560932abbaaca0
prkim99@gmail.com <prkim99@gmail.com>
2022-08-21 20:20:04 +0900

    다이어리 조회 api 구현(다이어리 번호, 모든 다이어리, 작성자별 다이어리 리스트 조회 가능)

--- ardiary/src/main/java/com/army/ardiary/service/DiaryListService.java
+++ ardiary/src/main/java/com/army/ardiary/service/DiaryListService.java
@@ -5,27 +5,55 @@ import com.army.ardiary.dto.DiaryResponseDto;
 import com.army.ardiary.repository.DiaryRepository;
 import io.jsonwebtoken.Claims;
 import com.army.ardiary.service.TokenService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
+@RequiredArgsConstructor
 public class DiaryListService {
 
-    private DiaryRepository diaryRepository;
-    private TokenService tokenService;
+    private final DiaryRepository diaryRepository;
+    private final TokenService tokenService;
 
     public DiaryResponseDto listMine(String token){
         Claims claims= tokenService.getJwtContents(token);
-        int id= Integer.parseInt(claims.getSubject());
-        ArrayList<DiaryEntity> diaryEntities=diaryRepository.selectByWriter(id);
+        int userId= Integer.parseInt(claims.getSubject());
+        ArrayList<DiaryEntity> diaryEntities =diaryRepository.selectByWriter(userId);
         DiaryResponseDto diaryResponseDto= DiaryResponseDto.builder()
                 .diaryList(diaryEntities)
                 .build();
-
         return diaryResponseDto;
     }
 
+    public DiaryResponseDto listAll(){
+        ArrayList<DiaryEntity> diaryEntities =diaryRepository.selectAll();
+        DiaryResponseDto diaryResponseDto= DiaryResponseDto.builder()
+                .diaryList(diaryEntities)
+                .build();
+        return diaryResponseDto;
+    }
 
+public DiaryResponseDto listById(int id){
+    ArrayList<DiaryEntity> diaryEntities = new ArrayList<>();
+    DiaryEntity diaryEntity=diaryRepository.selectById(id);
+    diaryEntities.add(diaryEntity);
+    DiaryResponseDto diaryResponseDto= DiaryResponseDto.builder()
+            .diaryList(diaryEntities)
+            .build();
+    return diaryResponseDto;
+}
+
+/*    public DiaryResponseDto listLike(String token){
+        Claims claims= tokenService.getJwtContents(token);
+        int id= Integer.parseInt(claims.getSubject());
+
+        DiaryEntity diaryEntity=diaryRepository.selectById(id);
+        DiaryResponseDto diaryResponseDto= DiaryResponseDto.builder()
+                .diaryList(diaryEntities)
+                .build();
+        return diaryResponseDto;
+    }*/
 
 }
