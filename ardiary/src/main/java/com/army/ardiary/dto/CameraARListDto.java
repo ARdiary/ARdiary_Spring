0421b0a8ddd7c309c286b6a1e14b8992b74042ae
prkim99@gmail.com <prkim99@gmail.com>
2022-09-03 01:23:20 +0900

    cameraARListDto 형식 결정

--- ardiary/src/main/java/com/army/ardiary/dto/CameraARListDto.java
+++ ardiary/src/main/java/com/army/ardiary/dto/CameraARListDto.java
@@ -1,12 +1,15 @@
 package com.army.ardiary.dto;
 
 import com.army.ardiary.domain.entity.CameraAREntity;
+import com.army.ardiary.domain.entity.MarkerEntity;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.Map;
+
 @Data
 @RequiredArgsConstructor
 public class CameraARListDto {
-    ArrayList<CameraAREntity> cameraARList;
+    ArrayList<Map.Entry<CameraAREntity, MarkerEntity>> cameraARList;
 }
