456330a1bea024e273526c02bcbb3db0bba70642
prkim99@gmail.com <prkim99@gmail.com>
2022-09-02 22:17:36 +0900

    어노테이션 수정

--- ardiary/src/main/java/com/army/ardiary/dto/DiaryRequestDto.java
+++ ardiary/src/main/java/com/army/ardiary/dto/DiaryRequestDto.java
@@ -1,14 +1,16 @@
 package com.army.ardiary.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Date;
 import java.util.List;
 
 @Data
-@Builder
+@RequiredArgsConstructor
 public class DiaryRequestDto {
     String title;
     String content;
--- ardiary/src/main/java/com/army/ardiary/service/FileService.java
+++ ardiary/src/main/java/com/army/ardiary/service/FileService.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
-@NoArgsConstructor
 @RequiredArgsConstructor
 public class FileService {
 
