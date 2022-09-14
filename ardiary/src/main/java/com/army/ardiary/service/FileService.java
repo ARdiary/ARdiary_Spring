18f7bad025ac9ff95bb2c8e52c3d64fedf6c8800
prkim99@gmail.com <prkim99@gmail.com>
2022-09-15 01:41:27 +0900

    aws 버킷 객체 저장 코드 작성

--- ardiary/src/main/java/com/army/ardiary/service/FileService.java
+++ ardiary/src/main/java/com/army/ardiary/service/FileService.java
@@ -1,7 +1,14 @@
 package com.army.ardiary.service;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,13 +21,18 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
-@NoArgsConstructor
 @RequiredArgsConstructor
 public class FileService {
 
+    //s3버킷
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
     //기본 루트 주소. 나중에 aws s3 파서 변경할 예정
-    String uploadPath="C:\\Users\\prkim\\source\\repos\\ARdiary\\ARdiray_Spring\\ardiary\\src\\main\\resources\\files";
-    public String uploadFiles(MultipartFile[] uploadFiles){
+    public String uploadFiles(MultipartFile[] uploadFiles,String type){
+        //type은 해당 파일이 timecapsule,diary, guestbook 중 어느 폴더에 저장될지 결정
         ArrayList<String> filePaths=new ArrayList<String>();
         //파일 비었는지 확인
         if( uploadFiles!=null){
@@ -32,14 +44,16 @@ public class FileService {
                 //UUID: 범용 고유 식별자
                 String uuid = UUID.randomUUID().toString();
                 //lastIndexOf("__")앞까지가 uuid임. 그 이후는 서버로 전송된 상태의 파일 이름
-                String savefileName = uploadPath + File.separator + uuid + "__" + fileName;
+                String savefileName = type+ File.separator + uuid + "__" + fileName;
 
-                Path savePath = Paths.get(savefileName);
-                try {
-                    //multipart 파일을 해당 위치에 저장(write)
-                    file.transferTo(savePath);
+                 try {
+                    ObjectMetadata objMeta = new ObjectMetadata();
+                    //파일 사이즈 읽어와 objectmetadata 세팅
+                    objMeta.setContentLength(file.getInputStream().available());
+                    //파일 s3에 저장
+                    amazonS3.putObject(bucket, savefileName, file.getInputStream(), objMeta);
                     //파일의 저장 위치 리스트에 추가
-                    filePaths.add(savePath.toString());
+                     filePaths.add(amazonS3.getUrl(bucket,savefileName).toString());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
