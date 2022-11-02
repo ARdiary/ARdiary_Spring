95cdf333b39176ece4751a8c6dc989b9a297ae5d
prkim99@gmail.com <prkim99@gmail.com>
2022-11-02 22:33:29 +0900

    notification CRUD

---
+++ ardiary/src/main/java/com/army/ardiary/repository/NotificationRepository.java
@@ -0,0 +1,17 @@
+package com.army.ardiary.repository;
+
+import com.army.ardiary.domain.entity.MarkerImageEntity;
+import com.army.ardiary.domain.entity.NotificationEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+@Mapper
+public interface NotificationRepository {
+    int insert(NotificationEntity notificationEntity);
+    NotificationEntity selectById(int notificationId);
+    NotificationEntity selectByUser (int userId);
+    int delete(int notificationId);
+}
