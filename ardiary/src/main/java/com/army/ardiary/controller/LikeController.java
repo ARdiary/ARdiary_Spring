c2c0a52c363e7a85d04bf750d3fb859423256bce
dldmldlsy <dldmldlsy@naver.com>
2022-11-05 06:56:13 +0900

    방명록 좋아요 추가 api

--- ardiary/src/main/java/com/army/ardiary/controller/LikeController.java
+++ ardiary/src/main/java/com/army/ardiary/controller/LikeController.java
@@ -37,4 +37,15 @@ public class LikeController {
         return ResponseEntity.status(HttpStatus.OK).body("일기 좋아요 삭제");
     }
 
+    @PostMapping("/api/likes/guestbooks")
+    public ResponseEntity<?> addLikeGuestBook(@RequestHeader(value = "Authorization")String headerToken, @RequestBody int id){
+        String token = headerToken.substring("Bearer ".length());
+        int userId = tokenService.findUserIdByJwt(token);
+        if(token == null|| !tokenService.validateToken(token))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse("토큰 인증 실패"));
+
+        likeService.addLikeGuestBook(userId, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body("방명록 좋아요 추가");
+    }
+
 }
--- ardiary/src/main/java/com/army/ardiary/service/LikeService.java
+++ ardiary/src/main/java/com/army/ardiary/service/LikeService.java
@@ -1,9 +1,12 @@
 package com.army.ardiary.service;
 
-import com.army.ardiary.domain.entity.DiaryEntity;
+import com.army.ardiary.domain.entity.GuestBookEntity;
 import com.army.ardiary.domain.entity.LikeDiaryEntity;
+import com.army.ardiary.domain.entity.LikeGuestBookEntity;
 import com.army.ardiary.repository.DiaryRepository;
+import com.army.ardiary.repository.GuestBookRepository;
 import com.army.ardiary.repository.LikeDiaryRepository;
+import com.army.ardiary.repository.LikeGuestBookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +14,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeDiaryRepository likeDiaryRepository;
+    private final LikeGuestBookRepository likeGuestBookRepository;
     private final DiaryRepository diaryRepository;
+    private final GuestBookRepository guestBookRepository;
     public void addLikeDiary(int userId, int diaryId){
         LikeDiaryEntity likeDiaryEntity = LikeDiaryEntity.builder()
                         .userId(userId)
@@ -26,5 +31,19 @@ public class LikeService {
     public void deleteLikeDiary(int userId, int diaryId){
         likeDiaryRepository.delete(userId, diaryId);
         //해당 다이어리 좋아요 수 -1
+    }
+
+    public void addLikeGuestBook(int userId, int guestBookId){
+        LikeGuestBookEntity likeGuestBookEntity = LikeGuestBookEntity.builder()
+                .userId(userId)
+                .guestBookId(guestBookId)
+                .build();
+
+        likeGuestBookRepository.insert(likeGuestBookEntity);
+
+        GuestBookEntity guestBookEntity = guestBookRepository.selectById(guestBookId);
+
+        int orig = guestBookEntity.getLikeNum();
+        guestBookEntity.setLikeNum(orig+1);
     }
 }
