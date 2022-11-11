168e3620856d2abfea8a7549a4ea5fdb3c57fef3
prkim99@gmail.com <prkim99@gmail.com>
2022-11-11 10:27:22 +0900

    알림 삭제/조회 기능 추가, 알링 생성관련 기능 추가

--- ardiary/src/main/java/com/army/ardiary/service/NotificationService.java
+++ ardiary/src/main/java/com/army/ardiary/service/NotificationService.java
@@ -1,35 +1,83 @@
 package com.army.ardiary.service;
 
-import com.army.ardiary.domain.entity.NotificationEntity;
-import com.army.ardiary.repository.NotificationRepository;
+import com.army.ardiary.domain.entity.*;
+import com.army.ardiary.repository.*;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+    private final FollowRepository followRepository;
+private final DiaryRepository diaryRepository;
+private final GuestBookRepository guestBookRepository;
 
-    List<NotificationEntity> findByUser(int userId){
+    public List<NotificationEntity> findByUser(int userId){
         List<NotificationEntity> notificationEntities=notificationRepository.selectByUser(userId);
         return notificationEntities;
     }
-    NotificationEntity findById(int notificationId){
+    public NotificationEntity findById(int notificationId){
         return notificationRepository.selectById(notificationId);
     }
-    void createNewFollowerNotification(){
 
-    }
-    void createNewDiaryOfFollowerNotification(){
+    public int deleteNotification(int notificationId){
 
+        return notificationRepository.delete(notificationId);
     }
-    void createNewGuestBookOfFollowerNotification(){
+    public boolean isUser(int userId, int notificationId){
+        return userId==notificationRepository.selectById(notificationId).getUserId();
+    }
+    public void createNewFollowerNotification(int followerId, int followeeId){
+        String followerNickname=userRepository.selectById(followerId).getNickname();
+        NotificationEntity newNotificationEntity=NotificationEntity.builder()
+                .userId(followeeId).notice(followerNickname+"님이 회원님을 팔로우하기 시작했습니다")
+                .build();
+        notificationRepository.insert(newNotificationEntity);
+    }
+    public void createTimecapsuleInvitationNotification(TimeCapsuleEntity timeCapsuleEntity, List<ParticipantEntity> participants){
 
+        for(ParticipantEntity participantEntity: participants){
+            String writerNickname=userRepository.selectById(timeCapsuleEntity.getWriter()).getNickname();
+            NotificationEntity newNotificationEntity=NotificationEntity.builder()
+                    .userId(participantEntity.getUserId())
+                    .notice(writerNickname+"님으로부터 타임캡슐 '"+timeCapsuleEntity.getTitle()+"' 에 초대되었습니다")
+                    .build();
+            notificationRepository.insert(newNotificationEntity);
+        }
     }
-    void createTimecapsuleInvitationNotification(){
+    public void createTimecapsuleOpenNotification(TimeCapsuleEntity timeCapsuleEntity,List<ParticipantEntity> participants){
+        for(ParticipantEntity participantEntity: participants){
+            String writerNickname=userRepository.selectById(timeCapsuleEntity.getWriter()).getNickname();
+            NotificationEntity newNotificationEntity=NotificationEntity.builder()
+                    .userId(participantEntity.getUserId())
+                    .notice(writerNickname+"타임캡슐 '"+timeCapsuleEntity.getTitle()+"' 이 오픈되었습니다")
+                    .build();
+            notificationRepository.insert(newNotificationEntity);
+        }
+    }
+    public void createLikedNotification(LikeDiaryEntity likeDiaryEntity ){
+        String nickname=userRepository.selectById(likeDiaryEntity.getUserId()).getNickname();
+        DiaryEntity diaryEntity=diaryRepository.selectById(likeDiaryEntity.getDiaryId());
+        NotificationEntity newNotificationEntity=NotificationEntity.builder()
+                .userId(diaryEntity.getWriter())
+                .notice(nickname+"님이 회원님의 일기'"+diaryEntity.getTitle()+"'을(를) 좋아합니다" )
+                .build();
+        notificationRepository.insert(newNotificationEntity);
+    }
+    public void createLikedNotification(LikeGuestBookEntity likeGuestBookEntity ){
+        String nickname=userRepository.selectById(likeGuestBookEntity.getUserId()).getNickname();
+        GuestBookEntity guestBookEntity=guestBookRepository.selectById(likeGuestBookEntity.getGuestBookId());
+        NotificationEntity newNotificationEntity=NotificationEntity.builder()
+                .userId(guestBookEntity.getWriter())
+                .notice(nickname+"님이 회원님의 방명록'"+guestBookEntity.getContent()+"'을(를) 좋아합니다" )
+                .build();
+        notificationRepository.insert(newNotificationEntity);
+    }
 
-    }
 }
